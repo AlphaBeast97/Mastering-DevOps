19a4948ba3673e01ac873f7585101fbcd557f169
AlphaBeast97 <ksaad5272@gmail.com>
2026-02-14 20:34:01 +0500

    Add initial application files with logging and test functionality

---
+++ application/index.js
@@ -0,0 +1,3 @@
+console.log("Hello DevOps!");
+console.log("This is a sample application to demonstrate DevOps practices.");
+console.log("Feel free to modify and enhance this application as needed.");
---
+++ application/test.js
@@ -0,0 +1,5 @@
+console.log("Starting tests for the DevOps application...");
+setTimeout(() => {
+  console.log("Waiting 3 seconds...");
+}, 3000);
+console.log("Tests completed!");
