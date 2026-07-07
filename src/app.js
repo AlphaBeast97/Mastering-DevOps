99941cda575922f2ad23a8f73d8fa4d051fb6e30
AlphaBeast97 <ksaad5272@gmail.com>
2026-07-07 16:35:07 +0500

    feat: enhance application with logging, security, and middleware improvements

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
 import express from "express";
+import logger from "#config/logger.js";
+import helmet from "helmet";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+app.use(helmet());
+app.use(
+  morgan("combined", {
+    stream: { write: (message) => logger.info(message.trim()) },
+  }),
+);
+app.use(cookieParser());
+
 app.get("/", (req, res) => {
-  res.send("Hello from mastering DevOps!");
+  logger.info("Hello from Acquisitions!");
+  res.send("Hello from Acquisitions!");
 });
 
 export default app;
